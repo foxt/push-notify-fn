09d0df1dfbb47720c5526e11a6b1d1f579767b60
foxt <me@foxt.dev>
2025-05-24 14:30:30 +0100

    move config to wrangler config

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Required by some browsers (namely Safari) otherwise notifications are rejected.
- * @see https://datatracker.ietf.org/doc/html/draft-thomson-webpush-vapid#section-2.1
- */
-export const VAPID_CONTACT_URI = "mailto:me+pushnotifyfn@foxt.dev"
-
-export const VAPID_PRIVATE_KEY: CryptoKey;
-export const VAPID_PUBLIC_KEY_B64: Uint8Array;
