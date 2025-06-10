e03c020303a011d75557a55474d75efa1d1cb9a5
foxt <me@foxt.dev>
2025-06-10 13:12:03 +0100

    Extra tidying up

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 addEventListener('push', event => {
     console.log(event);
-    let data = JSON.parse(event.data.text());
-    console.log(registration.showNotification(data.notification.title, data.notification));
+    const data = JSON.parse(event.data.text());
+    registration.showNotification(data.notification.title, data.notification);
 })
--- src/handleWebPushRequest.ts
+++ src/handleWebPushRequest.ts
@@ -4,16 +4,21 @@ import { WebPushRequest } from "./schemas.js";
 
 export async function handleWebPushRequest(request: Request, env: Env): Promise<Response> {
     let body = WebPushRequest.parse(await request.json());
+
+    // we do the ratelimit checks here to avoid people wasting their quota
+    // on invalid requests (i.e. when they're figuring out the API)
     let ratelimited = await env.ratelimit.limit({
         key: request.headers.get("cf-connecting-ip") || "unknown",
     })
     if (!ratelimited.success) return new Response("slow down!", { status: 429 });
+
+
     const config: WebPushConfig = {
         privateKey: await importECDSAKey(env.VAPID_PRIVATE_KEY_JWK),
         publicKeyB64: env.VAPID_PUBLIC_KEY_B64,
         vapidContactUri: env.VAPID_CONTACT_URI
     }
-
     await sendWebPush(body, config);
+
     return new Response(JSON.stringify({ok: true}), { status: 200 });
 }
